83e3a21c2f5e7a2fb16ee2704da1d30ebd486376
gpzim98 <gpzim98@gmail.com>
2015-06-16 00:24:27 -0300

    Iniciando os trabalhos

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
-setup(name='Django On OpenShift with Django 1.8',
+setup(name='avaliacao360',
     version='0.1.2',
-    description='django on OpenShift',
+    description='Projeto Avaliação 360',
     author='Gregory Pacheco',
     author_email='contato@gregorypacheco.com.br',
     url='https://github.com/Gpzim98/Django-OpenShift-1.8.git',
