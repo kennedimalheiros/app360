a6e43e29fa76360000be42eee963f247a97bb5ba
gpzim98 <gpzim98@gmail.com>
2015-06-16 00:17:30 -0300

    ..

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='Django On OpenShift with Django 1.8',
-    version='0.1.1',
+    version='0.1.2',
     description='django on OpenShift',
     author='Gregory Pacheco',
     author_email='contato@gregorypacheco.com.br',
