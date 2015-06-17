983cdb7b2b49c81befa5d1b3dfed2ad6350b1ceb
gpzim98 <gpzim98@gmail.com>
2015-06-16 22:33:26 -0300

    Fixing problem with static files link

--- project/urls.py
+++ project/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.conf import settings
-from django.conf.urls.static import static
 
 
 urlpatterns = [
     url(r'^$', 'project.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
