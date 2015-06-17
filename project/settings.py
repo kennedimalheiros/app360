5030dc8bb8e0b70da14767f1002a9e92ba88971d
gpzim98 <gpzim98@gmail.com>
2015-06-16 21:59:28 -0300

    Change settings to use Postgres local

--- project/settings.py
+++ project/settings.py
@@ -114,12 +114,15 @@ if ON_PAAS:
 
         
 else:
-    # stock django, local development.
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+            'default': {
+                'ENGINE': 'django.db.backends.postgresql_psycopg2',  
+                'NAME':     'avaliacao360',
+                'USER':     'postgres',
+                'PASSWORD': 'senha',
+                'HOST':     'localhost',
+                'PORT':     5432,
+            }
     }
 
 
