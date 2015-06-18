8c6a0f5803d9aa0b7a7860096c840a8ff03b3b2d
gpzim98 <gpzim98@gmail.com>
2015-06-17 23:00:48 -0300

    Problem possible solved

--- project/settings.py
+++ project/settings.py
@@ -161,4 +161,3 @@ TEMPLATE_DIRS = (
     # Don't forget to use absolute paths, not relative paths.
     os.path.join(BASE_DIR, 'templates'),
 )
-print ('9999999999999999999999999999999999 ') + os.path.join(BASE_DIR, 'templates')
