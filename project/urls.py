6a4d3304b56bb54cb1d5342fb8e1bc2532c4db65
gpzim98 <gpzim98@gmail.com>
2015-06-18 00:14:16 -0300

    Implements done of change password of django user

--- project/urls.py
+++ project/urls.py
@@ -12,31 +12,21 @@ urlpatterns = [
             template_name='registration/register.html',
             form_class=UserCreationForm,
             success_url='/',
-    )),
+    ), name='register',),
     url(r'^accounts/', include('django.contrib.auth.urls')),
-
-
-    url(r'^accounts/profile/$', 'project.views.dashboard',
-    name='url_dashborard'),
 
     url(r'^logout/$', 'django.contrib.auth.views.logout',
         {'template_name': 'registration/logout.html'}, name='url_logout'),
 
-    # Change Password URLs:
-    url(r'^accounts/password_change/$', 
-        'django.contrib.auth.views.password_change', 
-        {'post_change_redirect' : '/accounts/password_change/done/',
-        'template_name':'registration/password_change_form.html'}, 
-        name="password_change"), 
-    
-    url(r'^accounts/password_change/done/$', 
-        'django.contrib.auth.views.password_change_done'),
+    url(r'^accounts/profile/$', 'project.views.dashboard',
+    name='url_dashborard'),
 
-    #URL reset
-    url(r'^reset/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
-        'project.views.reset_confirm', name='reset_confirm'),
-    url(r'^reset/$', 'project.views.reset', name='reset'),
-    url(r'^reset_succeful/$', 'project.views.password_confirmation', name='url_reset_suceful'),
+    url(r'^change-password/$', 'django.contrib.auth.views.password_change', {
+    'template_name': 'registration/password_change_form.html'}, name="password-change"),
+
+    url(r'^change-password-done/$', 'django.contrib.auth.views.password_change_done', {
+    'template_name': 'registration/password_change_done.html'
+    }, name="password-change-done"),
 
     url(r'^admin/', include(admin.site.urls)),
 ]
