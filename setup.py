2d5c5aef086616454f1e33961b6d7cf0697e23a2
kennedimalheiros <kennedimalheiros@gmail.com>
2015-06-19 11:27:41 -0300

    alter parameters.

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 setup(name='avaliacao360',
     version='0.1.2',
     description='Projeto Avaliação 360',
-    author='Gregory Pacheco',
-    author_email='contato@gregorypacheco.com.br',
-    url='http://avaliacao360-gpzim98.rhcloud.com/',
+    author='Kennedi Malheiros',
+    author_email='kennedimalheiros@gmail.com',
+    url='http://app360-kennedimalheiros.rhcloud.com/',
     install_requires=['Django==1.8.2',],
 )
